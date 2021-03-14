29c98d322c6e4a3a1c9a99e2fde7ac77e9a33abd
Shivlal <shivlalchavan41@gmail.com>
2021-03-14 23:17:21 +0530

    second commit

--- app/src/main/java/com/app/stationaryapp/ui/BookListViewModel.kt
+++ app/src/main/java/com/app/stationaryapp/ui/BookListViewModel.kt
@@ -1,6 +1,5 @@
 package com.app.stationaryapp.ui
 
-import android.util.Log
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
@@ -8,7 +7,6 @@ import com.app.stationaryapp.model.Book
 import com.app.stationaryapp.repository.BookListRepository
 import com.app.stationaryapp.utils.DataState
 import com.app.stationaryapp.utils.exhaustive
-import com.google.gson.Gson
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -34,16 +32,11 @@ class BookListViewModel
                 bookListRepository.getBook()
                     .onEach {dataState->
                         _dataState.value = dataState
-
-                        var gson = Gson()
-                        Log.e("BookList","Data"+gson.toJson(_dataState.value))
                     }
                     .launchIn(viewModelScope)
              }
 
-             is BookStateEvent.None->{
-
-             }
+             is BookStateEvent.None->{}
            }.exhaustive
         }
     }
